998491e0eac31823346b7a96269efcfc1702108a
Max Loup <ndggbctyhb@privaterelay.appleid.com>
2026-08-30 12:21:14 +0200

    Home neu: News-Kurzüberblick + Mails-Kurzüberblick, Check-in-Streak ersetzt durch 'Letztes Training' (gleiche Optik), Satz zu Garmin-Veränderungen unten

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = 'friday-shell-v26';
+const SHELL = 'friday-shell-v28';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(['.', 'index.html', 'manifest.webmanifest'])));
