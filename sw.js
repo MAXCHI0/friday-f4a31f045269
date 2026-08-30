4800f6ad07d48a8d22dc3f40160ecb4929c19829
Max Loup <ndggbctyhb@privaterelay.appleid.com>
2026-08-30 12:11:34 +0200

    Makros voll Garmin-abhängig: Grundumsatz + Aktivität aus gemessenem Verbrauch, Trainings-/Ruhetag-Spanne, Kalorien folgen dem Garmin-Verbrauch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = 'friday-shell-v25';
+const SHELL = 'friday-shell-v26';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(['.', 'index.html', 'manifest.webmanifest'])));
