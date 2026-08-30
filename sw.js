4a35a856e4618074504de937a61c3f8444eb0242
Max Loup <ndggbctyhb@privaterelay.appleid.com>
2026-08-30 19:25:48 +0200

    Home: Mail-Kurzüberblick zeigt nur noch Uni-Mails (FH Aachen)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = 'friday-shell-v29';
+const SHELL = 'friday-shell-v30';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(['.', 'index.html', 'manifest.webmanifest'])));
